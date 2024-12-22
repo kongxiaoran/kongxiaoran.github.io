369846155edb4c941605658e37ec4d1caf9f0b29
eric <1254122559@qq.com>
2024-12-22 22:56:04 +0800

    Site updated: 2024-12-22 22:56:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/21/hello-world/"];function toRandomPost(){
+var posts=["2024/12/22/mac安装 picgo 报错 文件损坏/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
